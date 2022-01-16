10c8a84960e1529c09bd2a0e707fc8035dcbb5e2
Gayrat Usmonov <usmonovgayrat89@gmail.com>
2022-01-16 11:41:47 +0500

    fix calculation and tests

--- data/calculations.go
+++ data/calculations.go
@@ -1,20 +1,25 @@
 package data
 
 import (
+	"fmt"
 	"math/big"
 )
 
-func Fibonacci(x, y int64) string {
+func Fibonacci(x, y int64) (r string) {
 	a := big.NewInt(0)
 	b := big.NewInt(1)
 
-	var limit big.Int
-	limit.Exp(big.NewInt(10), big.NewInt(y), nil)
-	for a.Cmp(&limit) < 0 {
+	var i int64
+	for i = 0; i < y; i++ {
+		if i == x {
+			r = a.String()
+		} else if i > x {
+			r = fmt.Sprintf("%v, %v", r, a.String())
+		}
 		// Compute the next Fibonacci number, storing it in a.
 		a.Add(a, b)
 		// Swap a and b so that b is the next number in the sequence.
 		a, b = b, a
 	}
-	return a.String()
+	return r
 }
